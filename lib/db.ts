bb6337209a6d196fca7a0b488d8a152f76c283a8
zkLeonora <azkar986@gmail.com>
2026-09-23 23:51:03 +0700

    refactor(db): migrate database connection credentials to environment variables

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "ktfc4321",
-  port: 3307,
-  database: "manajemen_nilai",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 });
